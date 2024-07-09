cd26a6d194efa1b64abe1c92c43b6f1317ad89ca
Aditya Kendre <kendreaditya@Adityas-MacBook-Pro.local>
2024-07-09 16:24:35 -0400

    adjusted some text

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -25,11 +25,11 @@ export default function Header() {
           id="blob"
           className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
         ></div>
-        <div className="flex space-x-3 md:flex-row">
+        <div className="flex space-x-3 md:flex-row flex-col">
           <Image
             src="/akendre.jpeg"
             alt="Aditya Kendre"
-            className="w-16 h-16 rounded-full animate-bounce"
+            className="w-16 h-16 rounded-full animate-bounce m-auto md:m-0"
             width={64}
             height={64}
             priority
@@ -48,8 +48,8 @@ export default function Header() {
               >Bhaktivionda Thakur</StyledLink> */}
             </p>
             <p>
-              Scaling Saas + AI ideas to the moon while learning and building
-              cool things in NYC and SF. Founding team Semant, Gaudiya
+              Scaling ideas while learning and building
+              cool things. Founding team at Semant and Gaudiya
               Kirtan.
             </p>
 
