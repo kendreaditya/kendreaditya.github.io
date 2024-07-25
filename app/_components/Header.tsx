22368ee3412a6a99f5f06b2bb087f28c25a1120e
Aditya Kendre <kendreaditya@Adityas-MacBook-Pro.local>
2024-07-25 16:17:46 -0400

    fixed blob issue with light theme

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
       <div className="items-start justify-start w-full ">
         <div
           id="blob"
-          className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"
+          className="flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-to-br before:from-transparent before:to-blue-700 before:opacity-10 before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-900 after:via-[#0141ff] after:opacity-40 after:blur-2xl after:content-[''] before:lg:h-[360px] z-[-1]"
         ></div>
         <div className="flex space-x-3 md:flex-row flex-col">
           <Image
@@ -39,18 +39,17 @@ export default function Header() {
             <h1 className="text-lg font-bold">Aditya Kendre</h1>
 
             {/* <p */}
-              {/* id="mission-statment" */}
-              {/* className="mb-1 text-sm italic font-normal text-gray-500" */}
+            {/* id="mission-statment" */}
+            {/* className="mb-1 text-sm italic font-normal text-gray-500" */}
             {/* > */}
-              {/* jīve doyā, kṛṣṇa-nām—sarva-dharma-sār */}
-              {/* <StyledLink
+            {/* jīve doyā, kṛṣṇa-nām—sarva-dharma-sār */}
+            {/* <StyledLink
                 href="https://en.wikipedia.org/wiki/Bhaktivinoda_Thakur"
               >Bhaktivionda Thakur</StyledLink> */}
             {/* </p> */}
             <p>
-              Scaling ideas while learning and building
-              cool things. Founding team at Semant and Gaudiya
-              Kirtan.
+              Scaling ideas while learning and building cool things. Founding
+              team at Semant and Gaudiya Kirtan.
             </p>
 
             <div className="flex flex-row my-2 space-x-2">
