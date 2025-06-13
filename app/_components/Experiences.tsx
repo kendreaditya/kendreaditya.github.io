f8636d7bedea386ca25e328caa2a74c6707ca591
Aditya <kendreaditya@gmail.com>
2025-06-12 21:21:55 -0700

    Added some annimations to the experince images on hover

--- app/_components/Experiences.tsx
+++ app/_components/Experiences.tsx
@@ -1,9 +1,27 @@
 import data from "@/public/data.json";
 import { BriefcaseBusiness } from "lucide-react";
 
-const ExperienceNode = ({ company, image, description }: { company: string, image: any, description: string}) => (
+const ExperienceNode = ({
+  company,
+  image,
+  description,
+}: {
+  company: string;
+  image: any;
+  description: string;
+}) => (
   <div className="content-item">
-    <img className="item-image" src={image} alt={company} draggable={false} />
+    <img
+      className="item-image hover:rotate-[var(--rotation)] hover:scale-105 transition-all duration-200"
+      style={
+        {
+          "--rotation": `${Math.floor((Math.random()) * 30) - 15}deg`,
+        } as React.CSSProperties
+      }
+      src={image}
+      alt={company}
+      draggable={false}
+    />
     <div>
       <h3 className="item-title">{company}</h3>
       <p className="item-description">{description}</p>
